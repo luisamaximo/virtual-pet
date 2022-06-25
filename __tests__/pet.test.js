9a26e6702e0c74cd7006b50e2d72528dcd0645fc
Luisa Oliveira <luisamaximo@hotmail.com>
2022-06-25 13:36:40 +0100

    Getting old

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,6 +13,11 @@ describe('constructor', () => {
 
     expect(pet.age).toEqual(0);
   });
+  it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.hunger).toEqual(0);
+  });
   describe('growUp', () => {
     it('increments the age by 1', () => {
       const pet = new Pet('Fido');
@@ -20,5 +25,24 @@ describe('constructor', () => {
       pet.growUp();
   
       expect(pet.age).toEqual(1);
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(7);
     });
+    describe('walk', () => {
+      it('increments the fitness by 4', () => {
+        const pet = new Pet('Fido');
+    
+        pet.fitness = 4;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(8);
+      });
+      it('increases fitness by to a maximum of 10', () => {
+        const pet = new Pet('fido');
+    
+        pet.fitness = 8;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
+      });
 }); 
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,22 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
     this.growUp = function() {
         this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
     };
+    this.walk = function () {
+        if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+            this.fitness += 4;
+        } else {
+            this.fitness = MAXIMUM_FITNESS;
+        }
+    }
 };
 
 module.exports = Pet;
